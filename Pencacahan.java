7657ce68bfa6849f76725db4ae65afe5fdae7d37
syakirandien <128474861+syakirandien@users.noreply.github.com>
2023-03-23 13:18:45 +0700

    Update Pencacahan.java

--- Pencacahan.java
+++ Pencacahan.java
@@ -1,13 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication11;
+
+/**
+ *
+ * @author ASUS
+ */
+
+
 public class Pencacahan {
     private Keaktifan aktif;
     private StatusKunjungan statuskunjungan;
+
 
     public Pencacahan(Keaktifan aktif, StatusKunjungan statuskunjungan) {
         this.aktif = aktif;
         this.statuskunjungan = statuskunjungan;
     }
 
-    public Keaktifan getAktif() {
+    public Pencacahan(Keaktifan aktif) {
+        this.aktif = aktif;
+    }
+
+    public Pencacahan(StatusKunjungan statuskunjungan) {
+        this.statuskunjungan = statuskunjungan;
+    }
+
+    public Pencacahan() {
+    }
+    
+    public Keaktifan getKeaktifan() {
         return aktif;
     }
 
@@ -17,7 +41,9 @@ public class Pencacahan {
 
     @Override
     public String toString() {
-        return "Pencacahan{" + "aktif=" + aktif + ", statuskunjungan=" + statuskunjungan + '}';
+        return "\n<Keterangan Pencacahan>" + 
+                "\nKeaktifan\t: " + aktif.getValue() + 
+                "\nStatus Kunjungan: " + statuskunjungan.getValue();
     }
     
 }
